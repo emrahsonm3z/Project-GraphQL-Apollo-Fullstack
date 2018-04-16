3c891e98b2c6a8b49428a0bf8eaba9f2d1421140
Marc Bouchard <mab.bouchard@gmail.com>
2018-04-16 01:13:51 -0400

    refactor(components): move App.js to components directory

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,12 @@
 import React from 'react'
 
 // ------------------------------------ Relative Imports --
-import './App.css'
-import logo from './logo.svg'
+import '../styles/App.css'
 
 // ---------------------------------------- App Component --
 const App = () => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+    <h2>My first Apollo app js.span🚀</h2>
   </div>
 )
 
