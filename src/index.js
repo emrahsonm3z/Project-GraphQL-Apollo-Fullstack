12a2d36c872cce04e2fa491f4a7a218d27a4d957
Marc Bouchard <mab.bouchard@gmail.com>
2018-04-16 00:53:24 -0400

    feat(client): use Apollo client to query an endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 // -------------------------------------- Global Imports --
 import React from 'react'
 import { render } from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
 
 // ------------------------------------ Relative Imports --
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+})
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result))
 
 // -------------------------------- Render App Component --
 render(wrapApp(), document.getElementById('root'))
