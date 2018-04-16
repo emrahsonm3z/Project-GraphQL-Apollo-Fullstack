aa6cf4726ea10a16b338c00ee6b11421e082d66e
Marc Bouchard <mab.bouchard@gmail.com>
2018-04-16 02:04:01 -0400

    feat(Dogs): create Dogs component

---
+++ src/components/Dogs.js
@@ -0,0 +1,42 @@
+// -------------------------------------- Global Imports --
+import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { func } from 'prop-types'
+
+// ----------------------------------------------- Query --
+const GET_DOGS = gql`
+  {
+    dogs {
+      id
+      breed
+    }
+  }
+`
+
+// -------------------------------------- Dogs Component --
+export default function Dogs({ onDogSelected }) {
+  return (
+    <Query query={GET_DOGS}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading...'
+        if (error) return `Error! ${error.message}`
+
+        return (
+          <select name="dog" onChange={onDogSelected}>
+            {data.dogs.map(dog => (
+              <option key={dog.id} value={dog.breed}>
+                {dog.breed}
+              </option>
+            ))}
+          </select>
+        )
+      }}
+    </Query>
+  )
+}
+
+// ------------------------------------------- PropTypes --
+Dogs.propTypes = {
+  onDogSelected: func.isRequired,
+}
