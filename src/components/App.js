234380edc233b720bcc60e230e2d2fab292a7314
Marc Bouchard <mab.bouchard@gmail.com>
2018-04-16 21:19:17 -0400

    feat(App): render DogPhoto component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 // --------------------------------------- Global Imports --
-import React from 'react'
+import React, { Component } from 'react'
 
 // ------------------------------------ Relative Imports --
 import '../styles/App.css'
 import Dogs from './Dogs'
+import DogPhoto from './DogPhoto'
 
 // ---------------------------------------- App Component --
-const App = () => (
-  <div className="App">
-    <h2>My first Apollo app!</h2>
-    <Dogs />
-  </div>
-)
+class App extends Component {
+  state = { selectedDog: null }
+
+  onDogSelected = ({ target }) => {
+    this.setState(() => ({ selectedDog: target.value }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Building Query components</h2>
+        {this.state.selectedDog && <DogPhoto breed={this.state.selectedDog} />}
+        <Dogs onDogSelected={this.onDogSelected} />
+      </div>
+    )
+  }
+}
 
 // ********************************************************
 export default App
